7bdebb6f2ba5b48e46325525f8f99f5140fed784
LeeSangBaek <msng0408@naver.com>
2023-09-26 00:21:47 +0900

    맵툴, Util 작업
    - MapTool 오브젝트 로드
    - Util 추가 ( List, Random, Distance )

--- Assets/02.Scripts/AStar/Editor/MapEditor.cs
+++ Assets/02.Scripts/AStar/Editor/MapEditor.cs
@@ -20,6 +20,8 @@ public class MapEditor : EditorWindow
 
     // Map Data
     public List<GameObject> prefabList = new List<GameObject>();
+    GameObject currentPrefab;
+    int selGridIdx;
 
 
 
@@ -33,10 +35,14 @@ public class MapEditor : EditorWindow
         window.Show();
         window.minSize = new Vector2(200, 315);
         window.titleContent = new GUIContent("MapEditor", "This is Custom MapEditor");
+
     }
-    private void OnEnable() // Editor Open
+    private void OnEnable() // Editor Open 
     {
         Debug.Log("Editor Open");
+        // Instance
+        Instance = this;
+
         // Style
         style = new GUIStyle(); // Basic
         //style.font = loadFont[0];
@@ -45,44 +51,68 @@ public class MapEditor : EditorWindow
         style.onHover.textColor = Color.blue;
         style = new GUIStyle(style);
 
-        // TODO : Font
-        //Font[] loadFont = Resources.LoadAll<Font>("Fonts/Select");
-        //Debug.Log(loadFont.Length == 1 ? loadFont[0].name : $"너무 많은 폰트가 선택 되어 있습니다. 현재 폰트는 {loadFont[0].name}입니다.");
-
-
-        // Header // TODO : Header Style
-        //headerStyle = new GUIStyle();
-        //headerStyle.font = loadFont[0];
-        //headerStyle.fontStyle = FontStyle.Bold;
-        //headerStyle.normal.textColor = Color.white;
-        //headerStyle.hover.textColor = Color.blue;
-        //headerStyle = new GUIStyle(headerStyle);
-
-
-        // Instance
-        Instance = this;
-
-        // Tool Setting
+        // Unity Tool Setting
         lasTool = Tools.current;
         Tools.current = Tool.None;
 
         // Scene GUI
-        SceneView.duringSceneGui += SceneGUI;
+        SceneView.duringSceneGui += SceneGUI; // Scene GUI Setting
     }
     private void OnDisable()
     {
-        Tools.current = lasTool;
-        SceneView.duringSceneGui -= SceneGUI;
+        Debug.Log("Disable");
+        Tools.current = lasTool; // 사용중이던 툴 재 설정해주기
+        SceneView.duringSceneGui -= SceneGUI; // 씬 GUI 설정 해제 (메모리)
     }
-    private void OnDestroy()
+    //private void OnDestroy() // -> Disable과 동일, but 범용성은 Disable이 더 높음으로 Disable 사용
+    //{
+    //    Debug.Log("Destroy");
+    //    Tools.current = lasTool;
+    //    SceneView.duringSceneGui -= SceneGUI;
+    //}
+
+    private void OnFocus() // Map Editor가 선택 되었을 경우
     {
-        Tools.current = lasTool;
-        SceneView.duringSceneGui -= SceneGUI;
+        Debug.Log("MapEditor Focusing");
+        LoadPrefab();
     }
-    private void OnGUI()
+    private void OnGUI() // GUI Code
     {
-        EditorGUILayout.BeginVertical();
-        
+        EditorGUILayout.BeginVertical(); // Vertical 시작
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos, false, false);
+        LabelField("Select a prefab:", style);
+        if(prefabList != null && prefabList.Count > 0)
+        {
+            GUIContent[] content = new GUIContent[prefabList.Count];
+            for(int i = 0; i < prefabList.Count; i++)
+            {
+                if(prefabList[i] != null && prefabList[i].gameObject.name != "") // 비어있지 않고, 이름이 있다면
+                {
+                    content[i] = new GUIContent(prefabList[i].gameObject.name, AssetPreview.GetAssetPreview(prefabList[i].gameObject));
+                }
+
+                if(content[i] == null) // 만약 넣었는데, 비어있다면
+                    content[i] = GUIContent.none; // none
+            }
+
+            EditorGUI.BeginChangeCheck(); // 변화 체크 시작
+
+            selGridIdx = GUILayout.SelectionGrid(selGridIdx, content, 5
+            , GUILayout.Height(50 * (Mathf.Ceil(prefabList.Count / (float) 5)))
+            , GUILayout.Width(this.position.width - 30));
+
+            if(EditorGUI.EndChangeCheck())
+            {
+                // 변경 시 코드 
+            }
+            currentPrefab = prefabList[selGridIdx];
+
+            Texture2D previewImage = AssetPreview.GetAssetPreview(currentPrefab);
+            GUILayout.Box(previewImage);
+        }
+        EditorGUILayout.EndScrollView();
+
+        selGridIdx = EditorGUILayout.IntField(selGridIdx, style);
 
         EditorGUILayout.EndVertical();
     }
@@ -123,6 +153,8 @@ public class MapEditor : EditorWindow
                 }
         }
 
+        
+
         Repaint();
     }
 
@@ -130,6 +162,10 @@ public class MapEditor : EditorWindow
     {
         prefabList = SUtil.GetList(Resources.LoadAll<GameObject>("Prefabs/"));
     }
+
+
+
+    
     // Label Func
     private void LabelField(string label, GUIStyle style)
     {
--- Assets/02.Scripts/Common/SUtil.cs
+++ Assets/02.Scripts/Common/SUtil.cs
@@ -2,15 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using System;
 
 public static class SUtil
 {
-    public static List<T> GetList<T>(in List<T> originList)
+    // Random
+    public static int GetRandomIntValue(int start, int end)
+    {
+        return UnityEngine.Random.Range(start, end);
+    }
+    public static float GetRandomFloatValue(float start, float end)
+    {
+        return UnityEngine.Random.Range(start, end);
+    }
+    public static bool CheckRandom(float _value) // 0-1사이
+    {
+        float rand = GetRandomFloatValue(0f, 1f);
+        return rand > (1f - _value);
+    }
+    public static bool CheckRandom(int _value) // 1-100사이
+    {
+        int rand = GetRandomIntValue(0, 100);
+        return rand > (100 - _value);
+    }
+
+
+
+
+
+
+    // List 관련 함수
+    public static List<T> GetList<T>(in List<T> originList) // GetList By List
     {
         return originList.ToList();
     }
-    public static List<T> GetList<T>(in T[] originArray)
+    public static List<T> GetList<T>(in T[] originArray) // GetList By Array
     {
         return originArray.ToList();
     }
+    public static T GetRandomElement<T>(List<T> targetList) // Get Random Element At List
+    {
+        return targetList[GetRandomIntValue(0, targetList.Count)];
+    }
+    public static T GetRandomElement<T>(List<T> targetList, Predicate<T> predicate) // Get Random Element At Condition // 이건 더 공부해보기
+    {
+        List<T> targetNewList = GetList(targetList).FindAll(predicate);
+        return targetNewList[GetRandomIntValue(0, targetNewList.Count)];
+    }
+
+
+
+
+    // Distance 관련 + 추가 공부 필요
+    public static float GetDistance(Vector3 a, Vector3 b) // 거리 값 가져오기 (정확한 거리 값 계산)
+    {
+        return (a-b).magnitude;
+    }
+    public static float GetSqrDistance(Vector3 a, Vector3 b) // 거리값의 제곱값 가져오기 (단순 거리 비교)
+    {
+        return (a-b).sqrMagnitude;
+    }
+    public static bool CheckDistance(Vector3 a, Vector3 b, float distance) // 거리를 비교할 때엔 제곱값을 활용
+    {
+        return GetSqrDistance(a,b) <= distance * distance;
+    }
+    //public static float GetSqrDistance(Vector3 a, Vector3 b)
+    //{
+    //    return Mathf.Pow(b.x - a.x, 2) + Mathf.Pow(b.y - a.y, 2);
+    //}
+
+
+    // Position 관련
 }
